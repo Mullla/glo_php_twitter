d629d4d461133e9a48f58d432904c62e2aaa3f37
Mars <diablo_13_06@mail.ru>
2021-11-12 18:13:35 +0300

    Delete twitter directory

--- twitter/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once 'includes/functions.php';
-
-$posts = get_posts();
-
-include_once 'includes/header.php';
-include_once 'includes/tweet_form.php';
-include_once 'includes/posts.php';
-include_once 'includes/footer.php';
