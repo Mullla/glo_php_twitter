bee83a791333585c06e355b89188da5f217a248e
Mars <diablo_13_06@mail.ru>
2021-11-12 18:13:18 +0300

    Add files via upload

---
+++ twitter/includes/posts.php
@@ -0,0 +1,44 @@
+<?php if ($posts) { ?>
+
+  <section class="wrapper">
+    <ul class="tweet-list">
+      <?php foreach ($posts as $post) { ?>
+
+        <li>
+          <article class="tweet">
+            <div class="row">
+              <img class="avatar" src="<?= get_url($post['avatar']) ?>" alt="Аватар пользователя <?= $post['name'] ?>">
+              <div class="tweet__wrapper">
+                <header class="tweet__header">
+                  <h3 class="tweet-author"><?= $post['name'] ?>
+                    <a href="<?= get_url('user_posts.php?id=' . $post['user_id']) ?>" class="tweet-author__add tweet-author__nickname">@<?= $post['login'] ?></a>
+                    <time class="tweet-author__add tweet__date"><?= date('d.m.y в H:i', strtotime($post['date'])) ?></time>
+                  </h3>
+                  <button class="tweet__delete-button chest-icon"></button>
+                </header>
+                <div class="tweet-post">
+                  <p class="tweet-post__text"><?= $post['text'] ?></p>
+
+                  <?php if ($post['image']) { ?>
+                    <figure class="tweet-post__image">
+                      <img src="<?= $post['image'] ?>" alt="<?= $post['text'] ?>">
+                    </figure>
+                  <?php } ?>
+
+                </div>
+              </div>
+            </div>
+            <footer>
+              <button class="tweet__like">53</button>
+              <!-- tweet__like_active -->
+            </footer>
+          </article>
+        </li>
+
+      <?php } ?>
+    </ul>
+  </section>
+
+<?php } else {
+  echo "<h2>Постов не найдено</h2>";
+} ?>
---
+++ twitter/register.php
@@ -0,0 +1,5 @@
+<?php
+
+include_once 'includes/header.php';
+include_once 'includes/register_form.php';
+include_once 'includes/footer.php';
